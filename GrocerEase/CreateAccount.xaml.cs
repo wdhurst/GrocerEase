690689290b2a21ce4d2cb9dd06fdd62487bb65a2
wdhurst <wdhyea@hotmail.com>
2018-03-08 13:48:19 -0700

    create account page added

---
+++ GrocerEase/CreateAccount.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GrocerEase
+{
+    public partial class CreateAccount : ContentPage
+    {
+        public CreateAccount()
+        {
+            InitializeComponent();
+            string email = EmailBox.Text;
+            string password = PasswordBox.Text;
+            string confirm = PassConfirmBox.Text;
+            Button create = CreateButton;
+        }
+    }
+}
