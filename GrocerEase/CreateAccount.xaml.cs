37b07ff72033b4c9066a8b40a6c472435b759fb9
wdhurst <wdhyea@hotmail.com>
2018-03-08 15:18:26 -0700

    modified c# file for createAccount page

--- GrocerEase/CreateAccount.xaml.cs
+++ GrocerEase/CreateAccount.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
-
 namespace GrocerEase
 {
     public partial class CreateAccount : ContentPage
@@ -12,7 +11,20 @@ namespace GrocerEase
             string email = EmailBox.Text;
             string password = PasswordBox.Text;
             string confirm = PassConfirmBox.Text;
-            Button create = CreateButton;
+            CreateButton.Clicked += CreateButton_Click;
+        }
+        void CreateButton_Click(Object sender, EventArgs e)
+        {
+            if (PasswordBox.Text == PassConfirmBox.Text)
+            {
+                //Also check if E-mail is unique?
+                //Send Confirmation email and show confirmation page
+            }
+            else
+            {
+                //Alert user to reenter passwords and/or use an email that isn't 
+                //already in system
+            }
         }
     }
 }
