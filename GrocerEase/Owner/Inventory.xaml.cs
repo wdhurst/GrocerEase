06a0d85542cda7072c34c5870bc6cbd363323a41
jeanaaltura <35641148+jeanaaltura@users.noreply.github.com>
2018-04-04 20:31:28 -0600

    part of owner side

---
+++ GrocerEase/Owner/Inventory.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GrocerEase.Owner
+{
+    public partial class Inventory : ContentPage
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+            imgLogo.Source = ImageSource.FromResource("GrocerEase.store.png");
+            imgInventory.Source = ImageSource.FromResource("GrocerEase.inventory.png");
+            imgNotifs.Source = ImageSource.FromResource("GrocerEase.notifications.png");
+
+            //Tap Gesture Recognizer  
+            var LayoutTap = new TapGestureRecognizer();
+            LayoutTap.Tapped += (sender, e) => {
+                DefaultBackground();
+                App.Current.MainPage = new NavigationPage(new OwnerStoreLayout());
+                stckLayout.BackgroundColor = Color.Teal;
+            };
+            stckLayout.GestureRecognizers.Add(LayoutTap);
+            var NewListTap = new TapGestureRecognizer();
+            NewListTap.Tapped += (sender, e) => {
+                DefaultBackground();
+                App.Current.MainPage = new NavigationPage(new Inventory());
+                stckInventory.BackgroundColor = Color.Teal;
+            };
+            stckInventory.GestureRecognizers.Add(NewListTap);
+            var NotifsTap = new TapGestureRecognizer();
+            NotifsTap.Tapped += (sender, e) => {
+                DefaultBackground();
+                App.Current.MainPage = new NavigationPage(new Reminders());
+                stckNotifs.BackgroundColor = Color.Teal;
+            };
+            stckNotifs.GestureRecognizers.Add(NotifsTap);
+        }
+        public void DefaultBackground()
+        {
+            stckLayout.BackgroundColor = Color.White;
+            stckInventory.BackgroundColor = Color.White;
+            stckNotifs.BackgroundColor = Color.White;
+        }
+    }
+}
