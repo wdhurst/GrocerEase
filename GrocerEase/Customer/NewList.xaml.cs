11d127af973f9515252c78c901810fccc10c5223
Mitchell Bohn <mwbohn@mavs.coloradomesa.edu>
2018-04-10 12:07:51 -0600

    white icons, random styling, etc

--- GrocerEase/Customer/NewList.xaml.cs
+++ GrocerEase/Customer/NewList.xaml.cs
@@ -16,37 +16,24 @@ namespace GrocerEase
             imgNewList.Source = ImageSource.FromResource("GrocerEase.new-list.png");
             imgNotifs.Source = ImageSource.FromResource("GrocerEase.notifications.png");
             imgHome.Source = ImageSource.FromResource("GrocerEase.home.png");
+
             //Tap Gesture Recognizer  
             var LayoutTap = new TapGestureRecognizer();
             LayoutTap.Tapped += (sender, e) =>
             {
-                DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new StoreLayout());
-                stckLayout.BackgroundColor = Color.Teal;
             };
             stckLayout.GestureRecognizers.Add(LayoutTap);
-            var NewListTap = new TapGestureRecognizer();
-            NewListTap.Tapped += (sender, e) =>
-            {
-                DefaultBackground();
-                App.Current.MainPage = new NavigationPage(new NewList());
-                stckNewList.BackgroundColor = Color.Teal;
-            };
-            stckNewList.GestureRecognizers.Add(NewListTap);
             var NotifsTap = new TapGestureRecognizer();
             NotifsTap.Tapped += (sender, e) =>
             {
-                DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new Reminders());
-                stckNotifs.BackgroundColor = Color.Teal;
             };
             stckNotifs.GestureRecognizers.Add(NotifsTap);
             var HomeTap = new TapGestureRecognizer();
             HomeTap.Tapped += (sender, e) =>
             {
-                DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new CustomerHome());
-                stckHome.BackgroundColor = Color.Teal;
             };
             stckHome.GestureRecognizers.Add(HomeTap);
 
@@ -59,15 +46,6 @@ namespace GrocerEase
             // Reset the 'resume' id, since we just want to re-start here
             ((App)App.Current).ResumeAtShoppingListId = -1;
             listView.ItemsSource = await App.Database.GetItemsAsync();
-        }
-
-        public void DefaultBackground()
-        {
-            stckLayout.BackgroundColor = Color.White;
-            stckNewList.BackgroundColor = Color.White;
-            stckNotifs.BackgroundColor = Color.White;
-            stckHome.BackgroundColor = Color.White;
-
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
