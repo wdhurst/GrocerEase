c9b00d45532aaa3e850301389d52d147a9b4a2ff
ncampas <36755775+ncampas@users.noreply.github.com>
2018-04-17 02:46:11 -0600

    Still need work need help

--- GrocerEase/Owner/Inventory.xaml.cs
+++ GrocerEase/Owner/Inventory.xaml.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
-
 namespace GrocerEase.Owner
 {
     public partial class Inventory : ContentPage
     {
+
         public Inventory()
         {
+            brand.IsVisible = false;
+            cat.IsVisible = false;
+            aisle.IsVisible = false;
+            NumberOfItems.IsVisible = false;
+            Price.IsVisible = false;
+            addButton.IsVisible = false;
+
+            Title = "Category";
+            Padding = new Thickness(0, 20, 0, 0);
+            var listView = new ListView();
+            listView.ItemsSource = Data.CategoryList;
+            Content = listView; 
             InitializeComponent();
             imgLogo.Source = ImageSource.FromResource("GrocerEase.store.png");
             imgInventory.Source = ImageSource.FromResource("GrocerEase.inventory.png");
@@ -20,6 +32,48 @@ namespace GrocerEase.Owner
                 DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new OwnerStoreLayout());
                 stckLayout.BackgroundColor = Color.Teal;
+            };
+            NewIn.GestureReconizers.Add(NewTap);
+            var NewInventory = new TapGestureRecognizer();
+            NewInventory.Tapped += (sender, e) =>
+            {
+
+                //Make the brand category aisle number of items price and the add to inventory to appear and hide the load button
+                brand.IsVisible = true;
+                cat.IsVisible = true;
+                aisle.IsVisible = true;
+                NumberOfItems.IsVisible = true;
+                Price.IsVisible = true;
+                addButton.IsVisible = true;
+                LoadIn.IsVisible =false;
+
+            };
+            LoadIn.GestureReconizers.Add(LoadTap);
+            var LoadInvent = new TapGestureRecongnizer();
+            LoadInvent.Tapped += (sender, e) =>
+            {
+                //Load Inventory
+            };
+            addButton.GestureReconizer.Add(addInvent);
+            var addInventory = new TapGestureReconizer();
+            addInventory.Tapped += (sender, e) =>
+            {
+                brand.IsVisible = false;
+                cat.IsVisible = false;
+                aisle.IsVisible = false;
+                NumberOfItems.IsVisible = false;
+                Price.IsVisible = false;
+                addButton.IsVisible = false;
+                LoadIn.IsVisible = true;
+                //still need to add to inventory
+                async void addButton_Clicked(object sender, EventArgs e)
+                {
+                    await Navigation.PushAsync(new itemPage()
+                    {
+                        BindingContext = new ShoppingList()
+                    });
+                }
+
             };
             stckLayout.GestureRecognizers.Add(LayoutTap);
             var NewListTap = new TapGestureRecognizer();
