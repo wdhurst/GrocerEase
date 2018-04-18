06b7f538eb9eb17b36982e3d2871eb2dabe95731
Mitchell Bohn <mwbohn@mavs.coloradomesa.edu>
2018-04-18 01:52:16 -0600

    We can now move from Inventory->promotions
    to be continued

--- GrocerEase/Owner/Promotions.xaml.cs
+++ GrocerEase/Owner/Promotions.xaml.cs
@@ -17,31 +17,19 @@ namespace GrocerEase.Owner
             //Tap Gesture Recognizer  
             var LayoutTap = new TapGestureRecognizer();
             LayoutTap.Tapped += (sender, e) => {
-                DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new OwnerStoreLayout());
-                stckLayout.BackgroundColor = Color.Teal;
             };
-            stckLayout.GestureRecognizers.Add(LayoutTap);
+            //stckOwnerLayout.GestureRecognizers.Add(OwnerLayoutTap);
             var NewListTap = new TapGestureRecognizer();
             NewListTap.Tapped += (sender, e) => {
-                DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new Inventory());
-                stckInventory.BackgroundColor = Color.Teal;
             };
             stckInventory.GestureRecognizers.Add(NewListTap);
             var NotifsTap = new TapGestureRecognizer();
             NotifsTap.Tapped += (sender, e) => {
-                DefaultBackground();
                 App.Current.MainPage = new NavigationPage(new Promotions());
-                stckPromos.BackgroundColor = Color.Teal;
             };
             stckPromos.GestureRecognizers.Add(NotifsTap);
-        }
-        public void DefaultBackground()
-        {
-            stckLayout.BackgroundColor = Color.White;
-            stckInventory.BackgroundColor = Color.White;
-            stckPromos.BackgroundColor = Color.White;
         }
     }
 }
