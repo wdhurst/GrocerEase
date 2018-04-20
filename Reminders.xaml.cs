2e1b8b27164c42fbc1b20d2e3ec764faaef45d1d
ncampas <36755775+ncampas@users.noreply.github.com>
2018-04-20 10:58:00 -0600

    fix some changes?

--- Reminders.xaml.cs
+++ Reminders.xaml.cs
@@ -6,20 +6,19 @@ using Xamarin.Forms;
 
 namespace GrocerEase
 {
-    public partial class NewList : ContentPage
+    public partial class Reminders : ContentPage
     {
-        public NewList()
+        public Reminders()
         {
             InitializeComponent();
             addItem.Clicked += AddItem_Clicked;
             deleteList.Clicked += DeleteList_Clicked;
-            inCart.Clicked += InCart_Clicked;
-            allItems.Clicked += AllItems_Clicked;
+            activeRems.Clicked += ActiveRems_Clicked;
+            allRems.Clicked += AllRems_Clicked;
             imgLogo.Source = ImageSource.FromResource("GrocerEase.store.png");
             imgLists.Source = ImageSource.FromResource("GrocerEase.lists.png");
             imgNotifs.Source = ImageSource.FromResource("GrocerEase.notifications.png");
             imgHome.Source = ImageSource.FromResource("GrocerEase.home.png");
-            //imgItem.Source = ImageSource.FromResource("GrocerEase.new-list.png");
             //checkImg.Source = ImageSource.FromResource("GrocerEase.check.png");
 
 
@@ -30,45 +29,42 @@ namespace GrocerEase
                 App.Current.MainPage = new NavigationPage(new StoreLayout());
             };
             stckLayout.GestureRecognizers.Add(LayoutTap);
-            var NotifsTap = new TapGestureRecognizer();
-            NotifsTap.Tapped += (sender, e) =>
-            {
-                App.Current.MainPage = new NavigationPage(new Reminders());
+            var ListsTap = new TapGestureRecognizer();
+            ListsTap.Tapped += (sender, e) => {
+                App.Current.MainPage = new NavigationPage(new NewList());
             };
-            stckNotifs.GestureRecognizers.Add(NotifsTap);
+            stckLists.GestureRecognizers.Add(ListsTap);
             var HomeTap = new TapGestureRecognizer();
             HomeTap.Tapped += (sender, e) =>
             {
                 App.Current.MainPage = new NavigationPage(new CustomerHome());
             };
             stckHome.GestureRecognizers.Add(HomeTap);
-
-
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
             // Reset the 'resume' id, since we just want to re-start here
-            ((App)App.Current).ResumeAtShoppingListId = -1;
-            listView.ItemsSource = await App.DatabaseS.GetItemsAsync();
+            SaleListView.ItemsSource = null;
+            ((App)App.Current).ResumeAtReminderListId = -1;
+            listView.ItemsSource = await App.DatabaseR.GetItemsAsync();
         }
+
 
         async void AddItem_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new itemPage()
+            await Navigation.PushAsync(new addReminder()
             {
-                BindingContext = new ShoppingList()
+                BindingContext = new ReminderList()
             });
         }
 
         async void DeleteList_Clicked(object sender, EventArgs e)
         {
-            //if (App.Database.allInCart())
-                listView.ItemsSource = await App.DatabaseS.DeleteAllAsync();
-            //else
-              //  await DisplayAlert("Error", "Not all items in cart, clear list anyways?", "Yes", "No");
-                    
+            ((App)App.Current).ResumeAtPromotionId = -1;
+            listView.ItemsSource = null;
+            SaleListView.ItemsSource = await App.DatabaseP.GetItemsAsync();
         }
 
         async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
@@ -77,24 +73,39 @@ namespace GrocerEase
             //Debug.WriteLine("setting ResumeAtTodoId = " + (e.SelectedItem as TodoItem).ID);
             if (e.SelectedItem != null)
             {
-                await Navigation.PushAsync(new itemPage()
+                await Navigation.PushAsync(new addReminder()
                 {
-                    BindingContext = e.SelectedItem as ShoppingList
+                    BindingContext = e.SelectedItem as ReminderList
                 });
             }
 
         }
-        async void InCart_Clicked(object sender, EventArgs e)
+
+        void OnSaleListItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-           // ((App)App.Current).ResumeAtShoppingListId = -1;
-            listView.ItemsSource = await App.DatabaseS.allInCart();
+            if(e.SelectedItem != null)
+            {
+                BindingContext = e.SelectedItem as PromotionList;
+                var item = (PromotionList)BindingContext;
+                var message = item.ToString();
+                DisplayAlert("SALE", message, "OK");
+            }
 
         }
-        async void AllItems_Clicked(object sender, EventArgs e)
+
+       async void ActiveRems_Clicked(object sender, EventArgs e)
         {
-            ((App)App.Current).ResumeAtShoppingListId = -1;
-            listView.ItemsSource = await App.DatabaseS.GetItemsAsync();
+            ((App)App.Current).current = DateTime.Today;
+            SaleListView.ItemsSource = null;
+            listView.ItemsSource = await App.DatabaseR.timetoBuy(((App)App.Current).current);
         }
 
+        async void AllRems_Clicked(object sender, EventArgs e)
+        {
+            ((App)App.Current).ResumeAtReminderListId = -1;
+            SaleListView.ItemsSource = null;
+            listView.ItemsSource = await App.DatabaseR.GetItemsAsync();
+
+        }
     }
 }
